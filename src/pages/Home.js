701d23bc9526c7e9186fdee444b30c8bcfe96018
JoshuaNogues <120759313+JoshuaNogues@users.noreply.github.com>
2023-03-04 12:40:54 -0500

    added edit button on users own post

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,73 +1,88 @@
-import { ThemeContext } from "../context/theme.context"
-import { useContext, useEffect, useState } from "react"
-import { post, get } from "../services/authService"
-import { LoadingContext } from "../context/loading.context"
+import { ThemeContext } from "../context/theme.context";
+import { useContext, useEffect, useState } from "react";
+import { post, get } from "../services/authService";
+import { LoadingContext } from "../context/loading.context";
 
 const Home = () => {
+  const [postList, setPostList] = useState("");
+  const { user } = useContext(LoadingContext);
+  const { mode } = useContext(ThemeContext);
+  const [postText, setPostText] = useState("");
 
-    const [ postList, setPostList ] = useState('')
-    const { user } = useContext(LoadingContext)
-    const { mode } = useContext(ThemeContext)
-    const [postText, setPostText] = useState('');
+  const fetchPosts = async () => {
+    try {
+      const response = await get("/post");
+      setPostList(response.data);
+      console.log(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-
-       const fetchPosts = async () => {
-        try {
-            const response = await get('/post')
-            setPostList(response.data)
-            console.log(response.data)
-        } catch (err) {
-            console.log(err)
-        }
-       }
-
-       useEffect(()=> {
-        fetchPosts()
-       }, [])
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   const handlePost = (e) => {
-    
-    console.log('Posting:', postText);
+    console.log("Posting:", postText);
     setPostText(e.target.value);
   };
-    console.log(user)
+  console.log(user);
+
+  const isUserPost = (post) => {
+    return post.contributor._id === user._doc._id;
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    post(`/post/create-post/${user._doc._id}`, {postText})
-    .then((result)=> {
-        fetchPosts()
-        console.log(result.data)
-    })
-  }
+    post(`/post/create-post/${user._doc._id}`, { postText }).then((result) => {
+      fetchPosts();
+      console.log(result.data);
+    });
+  };
 
   return (
-    <div className={"Home " + mode}><h1>Timeline</h1>
+    <div className={"Home " + mode}>
+      <h1>Timeline</h1>
       <div className="post">
-      <img className="profile-pic" src="https://via.placeholder.com/50" alt="Profile" />
-      <textarea
-        className="post-text"
-        placeholder="What's on your mind?"
-        name="postText"
-        value={postText}
-        onChange={handlePost}
-      />
-      <button className="post-button" onClick={handleSubmit}>Post</button>
-    </div>
-
-    {postList.length ? postList.map((post) => {
-  return (
-    <div key={post._id}>
-      <img className="profile-pic" src="https://via.placeholder.com/50" alt="Profile" />
-      <p>{post.post}</p>
-      <p>Contributor: {post.contributor.username}</p>
+        <img
+          className="profile-pic"
+          src="https://via.placeholder.com/50"
+          alt="Profile"
+        />
+        <textarea
+          className="post-text"
+          placeholder="What's on your mind?"
+          name="postText"
+          value={postText}
+          onChange={handlePost}
+        />
+        <button className="post-button" onClick={handleSubmit}>
+          Post
+        </button>
+      </div>
+      {postList.length ? (
+        postList.map((post) => (
+          <div key={post._id} className="post-container">
+            <img
+              className="profile-pic"
+              src="https://via.placeholder.com/50"
+              alt="Profile"
+            />
+            <div className="post-details">
+              <h4>{post.contributor.username}</h4>
+              <div className="post-content">
+                <p>{post.post}</p>
+                {isUserPost(post) && <button>Edit</button>}
+              </div>
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>...Loading </p>
+      )}
     </div>
   );
-}) : <p>...Loading </p>}
+};
 
-    </div>
-
-  )
-}
-
-export default Home
+export default Home;
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,14 @@
 import { LoadingContext } from "../context/loading.context"
 import { useContext } from "react"
+import { ThemeContext } from "../context/theme.context"
 
 const Profile = () => {
 
     const {user} = useContext(LoadingContext)
+    const { mode } = useContext(ThemeContext)
 
     return (
-        <div>
+        <div className={"Home " + mode}>
             <h1>Profile</h1>
             {user && <p>Hi, {user._doc.firstName}</p>}
 
